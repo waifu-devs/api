03c4f522618bd101fce08cd9038abde3c8dd5cff
Juan Alvarez <juan@nemphi.com>
2024-09-02 13:34:40 -0500

    feat: basic authentication

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,16 @@
-import { index, integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { InferSelectModel } from "drizzle-orm";
+import { index, integer, sqliteTable, text, unique } from "drizzle-orm/sqlite-core";
 
 
 
 export const users = sqliteTable("users", {
 	id: text("id").primaryKey().notNull(),
+	githubId: integer("github_id"),
+	githubUsername: text("github_username")
+}, (table) => {
+	return {
+		users_github_id_unique: unique("users_github_id_unique").on(table.githubId)
+	}
 })
 
 export const sessions = sqliteTable("sessions", {
@@ -15,3 +22,6 @@ export const sessions = sqliteTable("sessions", {
 		sessions_user_id_idx: index("sessions_user_id_idx").on(table.user_id)
 	}
 })
+
+export type User = InferSelectModel<typeof users>
+export type Session = InferSelectModel<typeof sessions>
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,24 @@ import { Hono } from "hono"
 import { drizzle, LibSQLDatabase } from "drizzle-orm/libsql"
 import { createClient } from "@libsql/client/web"
 import * as schema from "./database"
+import { initializeLucia } from "./lucia"
+import { Lucia, Session, User } from "lucia"
+import { authRoute, setUserSession } from "./auth"
+import { HTTPException } from "hono/http-exception"
 
 type Bindings = {
 	DATABASE_URL: string;
 	DATABASE_AUTH_TOKEN: string;
+	GITHUB_CLIENT_ID: string;
+	GITHUB_CLIENT_SECRET: string;
 }
 
 type Variables = {
 	db: LibSQLDatabase<typeof schema>;
+	lucia: Lucia;
+
+	user: User | null;
+	session: Session | null;
 }
 
 export type C = {
@@ -19,21 +29,34 @@ export type C = {
 
 const app = new Hono<C>()
 
+app.onError(async (err, c) => {
+	if (err instanceof HTTPException) {
+		if (err.status >= 500) {
+			console.log(JSON.stringify({ message: err.message, status: err.status }))
+		}
+		return c.json({ message: err.message }, err.status)
+	}
+	return c.json({ message: err.message }, 500)
+})
+
 app.use(async (c, next) => {
 	const dbClient = createClient({ url: c.env.DATABASE_URL, authToken: c.env.DATABASE_AUTH_TOKEN })
 	const db = drizzle(dbClient, { schema })
+	const lucia = initializeLucia(dbClient)
 
 	c.set("db", db)
+	c.set("lucia", lucia)
 
-	await next()
+	return await next()
 })
 
+app.use(setUserSession);
+
+app.route("/auth", authRoute)
+
 app.get("/", async (c) => {
-	const db = c.get("db")
-	const results = await db.select().from(schema.users)
 	return c.json({
-		text: "Hello Waifu",
-		results,
+		text: "waifu.dev",
 	})
 })
 
