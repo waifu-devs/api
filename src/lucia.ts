fc469c3d76331a9f93bd46d8338ca0eb8cc13af0
Juan Alvarez <juan@nemphi.com>
2024-09-15 19:27:28 -0500

    fix: fix lucia

--- src/lucia.ts
+++ src/lucia.ts
@@ -1,14 +1,14 @@
-import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle"
+import { DrizzleSQLiteAdapter } from "@lucia-auth/adapter-drizzle"
 import { Lucia } from "lucia"
 import { Context } from "hono"
 import { C } from "."
 import { API_DOMAIN } from "./auth"
 import * as schema from "./database"
-import { PostgresJsDatabase } from "drizzle-orm/postgres-js"
+import { DrizzleD1Database } from "drizzle-orm/d1"
 
 
-export function initializeLucia(ctx: Context<C>, drizzle: PostgresJsDatabase<typeof schema>) {
-	const adapter = new DrizzlePostgreSQLAdapter(drizzle, schema.sessions, schema.users)
+export function initializeLucia(ctx: Context<C>, drizzle: DrizzleD1Database<typeof schema>) {
+	const adapter = new DrizzleSQLiteAdapter(drizzle, schema.sessions, schema.users)
 	const lucia = new Lucia(adapter, {
 		sessionCookie: {
 			attributes: {
